68fcf0be6d25116268d67f83ed46320c739f54ca
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2023-02-22 10:42:57 -0500

    secret is now cool
    - i am not going to list everything that i changed

--- secret/main.js
+++ secret/main.js
@@ -1,24 +1,33 @@
-var outputArea = document.getElementById("output");
+var before = document.getElementById("before");
 var inputArea = document.getElementById("inputBox");
+var terminal = document.getElementById("terminal");
 
 var inputValue;
 
-inputArea.focus();
+init();
 
-function command(cmd) {
-    switch(cmd) {
-        case 'help':
-            print("Help command used...");
-            break;
-        case 'back':
-            print("");
-            newTab("../index.html");
-            break;
-    }
+function init() {
+    printLine("welcome to the secret site!_", "greenGlow", 80);
+    inputArea.focus();
 }
 
-function print(output) {
-    outputArea.innerHTML += "<br>" + output;
+async function command(cmd) {
+    inputArea.value = "";
+    await printFancy("> " + inputValue, "whiteGlow", 10);
+    switch(cmd) {
+        case 'help':
+            printFancy("Help command used...", "greenGlow", 10);
+            break;
+        case 'back':
+            printFancy("Sending you back...", "redGlow", 10);
+            newTab("../index.html");
+            break;
+        case 'hello world':
+            printFancy("Hello World!", "greenGlow", 300);
+            break;
+        default:
+            printFancy("Command \'" + cmd + "\' unknown.", "redGlow", 10);
+    }
 }
 
 function typeIt(from, e) {
@@ -27,15 +36,57 @@ function typeIt(from, e) {
     if(kc == 13) {
         console.log(inputValue);
         command(inputValue);
-        inputArea.value = "";
     } else {
         inputValue = inputArea.value;
     }
 }
 
 function newTab(link) {
+setTimeout(function() {
+    window.open(link, "_self");
+}, 500);
+}
+
+function newElement(style) {
+    var next = document.createElement("p");
+        next.innerHTML = "";
+        next.className = style;
+        before.parentNode.insertBefore(next, before);
+        return next;
+}
+
+function print(text, element) {
+    element.innerHtml = text;
+}
+
+function printLine(text, style, time) {
     setTimeout(function() {
-      window.open(link, "_self");
-    }, 500);
-  }
-  
+        var next = document.createElement("p");
+        next.innerHTML = text;
+        next.className = style;
+
+        before.parentNode.insertBefore(next, before);
+
+        window.scrollTo(0, document.body.offsetHeight);
+    }, time);
+}
+
+async function printFancy(text, style, delay) {
+    var el = newElement(style);
+    addText(text, el, delay, 0);
+    terminal.scrollTo(0, terminal.offsetHeight);
+    await sleep(delay*text.length);
+    console.log("timeout over");
+    return;
+}
+
+async function addText(text, element, delay, index) {
+    if(index < text.length) {
+        element.innerHTML += text[index++];
+        setTimeout(function() {addText(text, element, delay, index)}, delay);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
