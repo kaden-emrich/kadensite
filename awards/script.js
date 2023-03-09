bf2b8748bbaba4a245863bcad7dcc339849a39b4
kaden-emrich <kadenemrich@gmail.com>
2023-03-09 12:26:02 -0500

    added awards dir
    - small changes to many things
    - created awards site with some (not all) of the awards that I have won.

---
+++ awards/script.js
@@ -0,0 +1,11 @@
+function mouseOn(el) {
+
+    el.style.animation = "lift .1s ease forwards";
+
+}// mouseOn(el)
+
+function mouseOff(el) {
+
+    el.style.animation = "drop .4s ease forwards";
+
+}// mouseOff(el)
