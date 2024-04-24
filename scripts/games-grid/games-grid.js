8420cdfb1e928222bc3cc008280fc0fdff1a9884
Kaden Emrich <kadenemrich@gmail.com>
2024-04-24 14:17:01 -0400

    fixed games background responsive event

--- scripts/games-grid/games-grid.js
+++ scripts/games-grid/games-grid.js
@@ -1,20 +1,20 @@
 let posters = document.querySelectorAll('.game-display img');
 
 posters.forEach((poster) => {
-    poster.onmouseover = () => {
+    poster.addEventListener('mouseover', () => {
         posters.forEach((p) => {
             p.classList.add('gd-no-hover');
         });
-    }
+    });
 
-    poster.onmouseleave = () => {
+    poster.addEventListener('mouseleave', () => {
         posters.forEach((p) => {
             p.classList.remove('gd-no-hover');
         });
-    }
+    });
 
-    poster.onclick = () => {
+    poster.addEventListener('click', () => {
         poster.classList.add('gd-fly-out');
         setTimeout(() => poster.classList.remove('gd-fly-out'), 500);
-    }
+    });
 });
